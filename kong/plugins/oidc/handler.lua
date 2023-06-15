37e6295af136159387a93bf25ecc9a4a9afbbaf8
Rui Peixoto <rui.f.peixoto@trustsystems.pt>
2023-06-15 12:55:41 +0100

    added cookies to header

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -156,8 +156,9 @@ function logout(oidcConfig, sessionConfig)
     --     return ngx.redirect("/")
     -- end
     ngx.log(ngx.DEBUG, " ******************** Logout Process **********************");
-    ngx.header['Set-Cookie'] =  "TMLCRM=; Max-Age=0; Expires=Thu, 1 Jan 1970 00:00:00 GMT; Path=/; Secure; HttpOnly; SameSite=Lax,";
-    ngx.header['Set-Cookie'] = "session=; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; HttpOnly; SameSite=Lax;"
+
+    ngx.header['Set-Cookie'] = "TMLCRM=; Max-Age=0; Expires=Thu, 1 Jan 1970 00:00:00 GMT; Path=/; Secure; HttpOnly; SameSite=Lax, session=; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; HttpOnly; SameSite=Lax;"
+
     return ngx.redirect("/")
 end
 

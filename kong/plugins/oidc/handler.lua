abf628f3730ad5e234cddf0ea4c7ff464420adb6
Rui Peixoto <rui.f.peixoto@trustsystems.pt>
2023-06-15 14:55:52 +0100

    Update handler.lua
    added cookies to logout complete

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -156,7 +156,7 @@ function logout(oidcConfig, sessionConfig)
     --     return ngx.redirect("/")
     -- end
     ngx.log(ngx.DEBUG, " ******************** Logout sucess **********************");
-    ngx.header['Set-Cookie'] =  "TMLCRM=; Max-Age=0; Expires=Thu, 1 Jan 1970 00:00:00 GMT; Path=/; HttpOnly; SameSite=Lax";
+    ngx.header['Set-Cookie'] =  {sessionConfig.jwt.cookie_name.."=; Max-Age=0; Expires=Thu, 1 Jan 1970 00:00:00 GMT; Path=/; HttpOnly; SameSite=Lax", "session=; Max-Age=0; Expires=Thu, 1 Jan 1970 00:00:00 GMT; Path=/; HttpOnly; SameSite=Lax"};
     return ngx.redirect("/")
 end
 

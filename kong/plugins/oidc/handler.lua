cf623252000141ff8527fecee4bc80cf14ec7fdc
Rui Peixoto <rui.f.peixoto@trustsystems.pt>
2023-06-15 13:03:06 +0100

    removing secure for the cookie

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -157,7 +157,7 @@ function logout(oidcConfig, sessionConfig)
     -- end
     ngx.log(ngx.DEBUG, " ******************** Logout Process **********************");
 
-    ngx.header['Set-Cookie'] = "TMLCRM=; Max-Age=0; Expires=Thu, 1 Jan 1970 00:00:00 GMT; Path=/; Secure; HttpOnly; SameSite=Lax, session=; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; HttpOnly; SameSite=Lax;"
+    ngx.header['Set-Cookie'] = "TMLCRM=; Max-Age=0; Expires=Thu, 1 Jan 1970 00:00:00 GMT; Path=/; SameSite=Lax, session=; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; SameSite=Lax;"
 
     return ngx.redirect("/")
 end

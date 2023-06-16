a07d35e3186371a76ee5d72b3a23c862c4954221
Rui Peixoto <rui.f.peixoto@trustsystems.pt>
2023-06-16 11:23:41 +0100

    try to fix bug

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -132,10 +132,13 @@ function getTokenfromCache(oidcConfig, sessionConfig, cookie_value, host, port, 
             ngx.log(ngx.DEBUG, " -*-*-* -*-*-* -*-*-*HTTP METHOD -*-*-* -*-*-* -*-*-*"  .. requestMethod);
             if requestMethod ~= "GET" then
                 ngx.log(ngx.DEBUG, "ENTERED XSRF VALIDATION");
+                
                 local xsrf = cache_get("session_jwt:".. cookie_value .. ":xsrf", host, port)
+                ngx.log(ngx.DEBUG, "xsrf   " .. xsrf);
+
                 local xsrf_cookie = ngx.var["cookie_XSRF-TOKEN"]
                 ngx.log(ngx.DEBUG, "xsrf_cookie " .. xsrf_cookie);
-                ngx.log(ngx.DEBUG, "xsrf   " .. xsrf);
+                
                 -- if(xsrf_cookie ~= xsrf) then
                 --     ngx.log(ngx.DEBUG, "xsrf   UNAUTHORIZED");
                 --     utils.exit(401, err, ngx.HTTP_UNAUTHORIZED)
@@ -160,7 +163,8 @@ function login(oidcConfig, sessionConfig)
         --cache_set("session_jwt:" .. uuid , token, sessionConfig.jwt.timeout, sessionConfig.redis.host, sessionConfig.redis.port)
         cache_set("session_jwt:" .. uuid , token, 43200, sessionConfig.redis.host, sessionConfig.redis.port)
         cache_set("session_jwt:".. uuid .. ":timestamp" , timeout, 43200, sessionConfig.redis.host, sessionConfig.redis.port)
-        
+        cache_set("session_jwt:".. uuid .. ":xsrf" , xsrf, 43200, sessionConfig.redis.host, sessionConfig.redis.port)
+
         --ngx.header['Set-Cookie'] =  sessionConfig.jwt.cookie_name.."=" .. uuid .. "; path=/"
 
         ngx.header['Set-Cookie'] =  {sessionConfig.jwt.cookie_name.."=" .. uuid .. "; path=/; HttpOnly; SameSite=Lax" , 
